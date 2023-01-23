3347ddb154679c8a8857046f5d4f63cbc028a080
shair1020 <ali99shair@gmail.com>
2023-01-24 01:02:21 +0500

    create a common components of form container and login components done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import Logo from "../assets/logo.svg";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { loginRoute } from "../utils/APIRoute";
+import { FormContainer } from "../styled-componet/FormContainer";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const navigate = useNavigate();
+  const [values, setValues] = useState({
+    username: "",
+    password: "",
+  });
 
-export default Login
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (handleValidation()) {
+      const { password, username } = values;
+      const { data } = await axios.post(loginRoute, {
+        username,
+        password,
+      });
+      if (data.status === false) {
+        toast.error(data.msg, toastOption);
+      }
+      if (data.status === true) {
+        localStorage.setItem("chat-app-user", JSON.stringify(data.user));
+        navigate("/");
+      }
+    }
+  };
+
+  const toastOption = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
+
+  const handleValidation = () => {
+    const { password, username } = values;
+    if (password === "") {
+      toast.error("Email and Password is required", toastOption);
+      return false;
+    } else if (username.length === "") {
+      toast.error("Email and Password is required", toastOption);
+      return false;
+    }
+    return true;
+  };
+
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+
+  return (
+    <>
+      <FormContainer>
+        <form onSubmit={(event) => handleSubmit(event)}>
+          <div className="brand">
+            <img src={Logo} alt="" />
+            <h1>Login</h1>
+          </div>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={(e) => handleChange(e)}
+            min="3"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => handleChange(e)}
+          />
+          <button type="submit">Login User</button>
+          <span>
+            Don't have an account ? <Link to="/register">register 🔥 </Link>
+          </span>
+        </form>
+      </FormContainer>
+      <ToastContainer />
+    </>
+  );
+};
+
+export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
-import styled from "styled-components";
 import Logo from "../assets/logo.svg";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
 import { resgisterRoute } from "../utils/APIRoute";
+import { FormContainer } from "../styled-componet/FormContainer";
 
 const Register = () => {
   const navigate = useNavigate();
@@ -110,72 +110,5 @@ const Register = () => {
     </>
   );
 };
-
-const FormContainer = styled.div`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 1rem;
-  align-items: center;
-  background-color: #131324;
-  .brand {
-    display: flex;
-    align-items: center;
-    gap: 1rem;
-    justify-content: center;
-    img {
-      height: 5rem;
-    }
-    h1 {
-      color: #fff;
-      text-transform: uppercase;
-    }
-  }
-  form {
-    display: flex;
-    flex-direction: column;
-    gap: 2rem;
-    background-color: #00000076;
-    border-radius: 2rem;
-    padding: 3rem 5rem;
-    input {
-      padding: 1rem;
-      background-color: transparent;
-      border: 0.1rem solid #4e0eff;
-      border-radius: 0.4rem;
-      color: #fff;
-      width: 100%;
-      font-size: 1rem;
-      &:focus {
-        border: 0.1rem solid #997af0;
-        outline: none;
-      }
-    }
-    button {
-      background-color: #997af0;
-      color: #fff;
-      padding: 1rem 2rem;
-      cursor: pointer;
-      border-radius: 0.4rem;
-      font-weight: bold;
-      text-transform: uppercase;
-      transition: 0.8ms ease-in-out;
-      &:hover {
-        background-color: #4e0eff;
-      }
-    }
-    span {
-      color: #fff;
-      text-transform: uppercase;
-      a {
-        color: #4e0eff;
-        text-decoration: none;
-        font-weight: bold;
-      }
-    }
-  }
-`;
 
 export default Register;
