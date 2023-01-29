ca831ab1fed1598d3d2d30ccb5f19f21f8b0bc4b
shair1020 <ali99shair@gmail.com>
2023-01-30 01:07:53 +0500

    WIP

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ChatContainer } from "../styled-componet/ChatContainer";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { allUsersRoute } from "../utils/APIRoute";
 
 const Chat = () => {
+  const navigate = useNavigate();
+  const [contacts, setContacts] = useState([]);
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  useEffect(() => {
+    if (!localStorage.getItem("chat-app-user")) {
+      navigate("/login");
+    } else {
+      setCurrentUser(JSON.parse(localStorage.getItem("chat-app-user")));
+    }
+  }, [navigate]);
+
+  useEffect(() => {
+    if (currentUser) {
+      if (currentUser.isAvatarImageSet) {
+        getAllUser(currentUser);
+      } else {
+        navigate("/setAvatar");
+      }
+    }
+  }, [navigate, currentUser]);
+
+  const getAllUser = async (currentUser) => {
+    const { data } = await axios.get(`${allUsersRoute }/${currentUser._id}}`);
+    console.log(data, "🔥");
+    setContacts(data.data);
+  };
   return (
     <ChatContainer>
-    <div className="container"></div>
-  </ChatContainer>
-  )
+      <div className="container"></div>
+    </ChatContainer>
+  );
 };
 
 export default Chat;
